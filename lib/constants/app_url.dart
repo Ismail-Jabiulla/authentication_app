e37c0e551a8b1fd9c7394fb1d6a82c3b2cf79329
MD. ISMAIL JABIULLA <41004552+Ismail-Jabiulla@users.noreply.github.com>
2024-01-22 23:41:23 +0600

    Update app_url.dart

--- lib/constants/app_url.dart
+++ lib/constants/app_url.dart
@@ -1,3 +1,3 @@
 
-const String authUrl = "https://testgeniepers.onrender.com/users/login";
-const String productUrl = "https://geniepers.onrender.com/community-images/all";
+const String authUrl = "https://testgeniepers.onrender.com/users/login/page";
+const String productUrl = "https://geniepers.onrender.com/community-images/all/page";
