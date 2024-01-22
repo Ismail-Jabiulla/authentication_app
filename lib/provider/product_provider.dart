79980c7d28c9f796154af7ffce7e3c7f75e79889
MD. ISMAIL JABIULLA <41004552+Ismail-Jabiulla@users.noreply.github.com>
2024-01-22 23:40:24 +0600

    Update product_provider.dart

--- lib/provider/product_provider.dart
+++ lib/provider/product_provider.dart
@@ -13,7 +13,7 @@ class ProductProvider extends ChangeNotifier{
 
   Future <void> fetchProductData() async {
 
-    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjZlNDM1YzM5NWRkMjU2ZWFkMmI1ZCIsImlhdCI6MTcwMTk1MDMzMSwiZXhwIjoxNzA0NTQyMzMxfQ.IaYreti55-nZSrRgML-csR44L-87kdped3n3ohmp3Lo";
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjZlNDM1YzM5NWRkMjU2ZWFkMmI1ZCIsImlhdCI6MTcwMTk1MDMzMSwiZXhwIjoxNzA0NTQyMzMxfQ.IaYreti55-nZSrRgML-csR44L-87kdped3n3ohmp3Lo99";
     print("fetchProductData");
     final response = await http.get(Uri.parse(productUrl),
     headers: {"Authorization":"Bearer $token"});
